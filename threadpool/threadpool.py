75201e6968a707e25337067ef0f5eeb5be051507
Michael Tsai <michaeltsai@qnap.com>
2016-08-25 15:01:56 +0800

    Use result pool to substitute queue method.

--- threadpool/threadpool.py
+++ threadpool/threadpool.py
@@ -15,47 +15,71 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.";
 #
 
-import sys
 import Queue
+import traceback
 import threading
+
+class ResultPool(object):
+    """Storage task result pool."""
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(ResultPool, cls).__new__(cls, *args, **kwargs)
+            cls._instance.pool = {}
+
+        return cls._instance
+
+    @classmethod
+    def list(cls):
+        return cls._instance.pool
+
+    @classmethod
+    def get(cls, key):
+        if key in cls._instance.pool:
+            return cls._instance.pool[key]
+
+        return None
+
+    @classmethod
+    def set(cls, key, val):
+        cls._instance.pool[key] = val
+
+    @classmethod
+    def clear(cls):
+        cls._instance.pool = {}
 
 
 class Worker(threading.Thread):
     """Routines for work thread."""
-    def __init__(self, in_queue, out_queue, err_queue):
+    def __init__(self, in_queue):
         """Initialize and launch a work thread,
         in_queue which tasks in it waiting for processing,
-        out_queue which the return value of the task in it,
-        err_queue which stores error info when processing the task.
+        result which store tasks' result.
         """
         threading.Thread.__init__(self)
         self.setDaemon(True)
         self.in_queue = in_queue
-        self.out_queue = out_queue
-        self.err_queue = err_queue
+        self.result = ResultPool()
         self.start()
 
     def run(self):
         while True:
             # Processing tasks in the in_queue until command is stop.
-            command, callback, args, kwds = self.in_queue.get()
-            if command == 'stop':
-                break
+            command, task_name, callback, args, kwds = self.in_queue.get()
             try:
+                if command == 'stop':
+                    break
                 if command != 'process':
                     raise ValueError('Unknown command %r' % command)
+
+                self.result.set(task_name, callback(*args, **kwds))
             except:
-                self.report_error()
-            else:
-                self.out_queue.put(callback(*args, **kwds))
+                self.result.set(task_name, {'err': traceback.format_exc()})
 
     def dismiss(self):
         command = 'stop'
         self.in_queue.put((command, None, None, None))
-
-    def report_error(self):
-        '''We "report" errors by adding error information to err_queue.'''
-        self.err_queue.put(sys.exc_info()[:2])
 
 
 class ThreadPool():
@@ -67,48 +91,46 @@ class ThreadPool():
         and initialize three queues.
         """
         # pool_size = 0 indicates buffer is unlimited.
-        num_threads = ThreadPool.max_threads \
+        self.num_threads = ThreadPool.max_threads \
             if num_threads > ThreadPool.max_threads \
             else num_threads
         self.in_queue = Queue.Queue(pool_size)
-        self.out_queue = Queue.Queue(pool_size)
-        self.err_queue = Queue.Queue(pool_size)
+        self.result = ResultPool()
+
+        self.init_workers()
+
+    def init_workers(self):
         self.workers = {}
-        for i in range(num_threads):
-            worker = Worker(self.in_queue, self.out_queue, self.err_queue)
+
+        for i in range(self.num_threads):
+            worker = Worker(self.in_queue)
             self.workers[i] = worker
 
-    def add_task(self, callback, *args, **kwds):
+    def restart(self):
+        self.destroy()
+        self.init_workers()
+
+    def add_task(self, task_name, callback, *args, **kwds):
         command = 'process'
-        self.in_queue.put((command, callback, args, kwds))
+        self.result.set(task_name, None)
+        self.in_queue.put((command, task_name, callback, args, kwds))
 
-    def _get_results(self, queue):
-        '''Generator to yield one after the others all items currently
-           in the queue, without any waiting
-        '''
-        try:
-            while True:
-                yield queue.get_nowait()
-        except Queue.Empty:
-            raise StopIteration
+    def get_task_result(self, task_name):
+        return self.result.get(task_name)
 
-    def get_task(self):
-        return self.out_queue.get()
-
-    def show_results(self):
-        for result in self._get_results(self.out_queue):
-            print 'Result:', result
-
-    def show_errors(self):
-        for etyp, err in self._get_results(self.err_queue):
-            print 'Error:', etyp, err
+    def get_all_task_result(self):
+        return self.result.list()
 
     def destroy(self):
-        # order is important: first, request all threads to stop...:
-        for i in self.workers:
-            self.workers[i].dismiss()
-        # ...then, wait for each of them to terminate:
-        for i in self.workers:
-            self.workers[i].join()
-        # clean up the workers from now-unused thread objects
-        del self.workers
+        if hasattr(self, 'workers'):
+            # Clear result pool.
+            self.result.clear()
+
+            # order is important: first, request all threads to stop...:
+            for i in self.workers:
+                self.workers[i].dismiss()
+            # ...then, wait for each of them to terminate:
+            for i in self.workers:
+                self.workers[i].join()
+            # clean up the workers from now-unused thread objects
+            del self.workers
